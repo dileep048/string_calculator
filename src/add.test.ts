7eee9932468152d8c8b17b41c4e5452aa0178035
Dileep <dileepchaudhary048@gmail.com>
2024-09-25 19:40:23 +0530

    test for number 1 and number 2

--- src/add.test.ts
+++ src/add.test.ts
@@ -3,3 +3,11 @@ import { add } from "./utils/add";
 test('return 0 for an empty string', () => {
     expect(add("")).toBe(0);
 })
+
+test('return number itself if 1 numbers', () => {
+    expect(add("1")).toBe(1)
+})
+
+test('return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3)
+})
--- src/utils/add.ts
+++ src/utils/add.ts
@@ -1,3 +1,5 @@
 export function add(numbers: string) {
-    // implementation will go here
+    if(numbers === ""){
+        return 0
+    }
 }
