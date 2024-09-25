9ca8b947a63800b1c5fcb76dce96162d002822d5
Dileep <dileepchaudhary048@gmail.com>
2024-09-25 20:55:31 +0530

    test error for negative number

--- src/add.test.ts
+++ src/add.test.ts
@@ -23,3 +23,15 @@ test('handle new lines between number', () => {
 test('support custom delimiter', () => {
     expect(add("//;\n1;2")).toBe(3);
 })
+
+test('throws error for single negative number', () => {
+    expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
+})
+
+test('throw erro for multiple negative numbers', () => {
+    expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed: -2, -4");
+})
+
+test('throws an error for negative numbers in custom delimiter', () => {
+    expect(() => add("//;\n1;2;-3")).toThrow("negative numbers not allowed: -3");
+});
