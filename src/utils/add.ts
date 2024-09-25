dc96dfcfbea1feaec44c4d0f2a2b3a6356f24b32
Dileep <dileepchaudhary048@gmail.com>
2024-09-25 20:16:45 +0530

    Add support for custom delimeter

--- src/utils/add.ts
+++ src/utils/add.ts
@@ -1,4 +1,9 @@
 export function add(numbers: string) {
     if(numbers === "") return 0;
+    if(numbers.startsWith("//")){
+        const delimiter = numbers[2];
+        numbers = numbers.slice(4);
+        return numbers.split(delimiter).reduce((sum,num) => sum + parseInt(num,10), 0);
+    }
     return numbers.split(/[\n,]/).reduce((sum,num) => sum + parseInt(num, 10), 0);
 }
