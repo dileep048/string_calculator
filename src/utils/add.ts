4c1a66bae96e985edd845fead6c88b6a2213958a
Dileep <dileepchaudhary048@gmail.com>
2024-09-25 21:02:42 +0530

    added error for negative numbers

--- src/utils/add.ts
+++ src/utils/add.ts
@@ -1,9 +1,18 @@
 export function add(numbers: string) {
-    if(numbers === "") return 0;
-    if(numbers.startsWith("//")){
-        const delimiter = numbers[2];
+    if (numbers === "") return 0;
+
+    let delimiter: RegExp | string = /[\n,]/;
+
+    if (numbers.startsWith("//")) {
+        delimiter = numbers[2];
         numbers = numbers.slice(4);
-        return numbers.split(delimiter).reduce((sum,num) => sum + parseInt(num,10), 0);
     }
-    return numbers.split(/[\n,]/).reduce((sum,num) => sum + parseInt(num, 10), 0);
+
+    // if negative numbers throw error
+    const negatives = numbers.split(delimiter).filter(num => parseInt(num, 10) < 0);
+    if (negatives.length > 0) {
+        throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
+    }
+
+    return numbers.split(delimiter).reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
